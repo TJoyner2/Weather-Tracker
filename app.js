76c43878daab34a3a8edfd7a817a72f847205e01
Trent <trent.joyner.gmail.com>
2024-04-25 21:47:55 -0400

    completly forgot about commits smh. everything is working except for the 'weather-icon'. fixed the API_KEY

--- app.js
+++ app.js
@@ -1,80 +1,87 @@
-const searchBtn = document.querySelector('#search')
-const APIkey = '6320828b522ee3cedf3185121e3e1811'
-const cityInput = document.querySelector('#city-input')
+const cityInput = document.querySelector(".city-input");
+const searchButton = document.querySelector(".search-btn");
+const weatherCardsDiv = document.querySelector(".weather-cards");
+const currentWeatherDiv = document.querySelector(".current-weather")
+const API_KEY = "6320828b522ee3cedf3185121e3e1811";
+const iconUrl = "https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@2x.png";
 
-function kToF(kelvin) {
-    return (kelvin * 9/5) - 459.67;
-}
-
-function createDayEl(weatherObj){
-    const div = document.createElement('div')
-    const date = document.createElement('p')
-    date.textContent = dayjs.unix(weatherObj.dt).format('M/D/YYYY')
-    div.appendChild(date)
-    const temp = document.createElement('p')
-    temp.textContent = `${kToF(weatherObj.main.temp).toFixed(1)} °F`
-    div.appendChild(temp)
-    const humidity = document.createElement('p')
-    humidity.textContent = `Humidity: ${weatherObj.main.humidity}`
-    div.appendChild(humidity)
-    const wind = document.createElement('p')
-    wind.textContent = `Wind: ${weatherObj.wind.speed}`
-    div.appendChild(wind)
-
-    return div
-}
-
-searchBtn.addEventListener('click', async()=> {
-    const inputCity = document.querySelector('#city-input').value
-
-    const cityResult = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${inputCity}&limit=1&appid=${APIkey}`)
-
-
-    const {lat, lon} = (await cityResult.json())[0];
-
-    console.log({lat, lon})
-
-    const forecastResult = await fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIkey}`)
-
-   
-    const weatherArr = (await forecastResult.json()).list.filter((el, i) => i % 8 === 0)
-
-    console.log(weatherArr)
-
-   const todayDiv = document.querySelector('#today')
-   const h2 = document.createElement('h2')
-   h2.textContent = inputCity
-   todayDiv.appendChild(h2)
-   todayDiv.appendChild(createDayEl(weatherArr[0]))
-
-
-   const fiveDay = document.querySelector('#five-day')
-   weatherArr.forEach(el => fiveDay.appendChild(createDayEl(el)))
-
-    //api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
-})
-
-function saveCityToLocalStorage(city) {
-    localStorage.setItem('lastCity', city);
-}
-
-function loadCityFromLocalStorage() {
-    return localStorage.getItem('lastCity');
-}
-
-searchBtn.addEventListener('click', async () => {
-    const inputCity = cityInput.value;
-
-    saveCityToLocalStorage(inputCity);
-})
-
-window.addEventListener('load', () => {
-    const lastCity = loadCityFromLocalStorage();
-    if (lastCity) {
-        cityInput.value = lastCity;
+const createWeatherCard = (cityName, weatherItem, index) => {
+    if (index === 0) { //single day weather
+        return `<div class="details">
+                    <h2>${cityName} (${weatherItem.dt_txt.split(" ")[0]})</h2>
+                    <h4>Temperature: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h4>
+                    <h4>Wind: ${weatherItem.wind.speed}M/S</h4>
+                    <h4>Humidity: ${weatherItem.main.humidity}%</h4> 
+                </div>
+                <div class="icon">
+                    <img src="${iconUrl}" alt="weather-icon">
+                    <h4>${weatherItem.weather[0].description}</h4>
+                </div>`;
+    } else {  //5 day weather
+        return `<li class="card">
+                    <h3>(${weatherItem.dt_txt.split(" ")[0]})</h3>
+                    <img src="${iconUrl}" alt="weather-icon">
+                    <h4>Temp: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h4>
+                    <h4>Wind: ${weatherItem.wind.speed}M/S</h4>
+                    <h4>Humidity ${weatherItem.main.humidity}%</h4> 
+                </li>`;
     }
-    if (lastCity) {
-        searchBtn.click();
-    }
-})
 
+}
+
+
+
+//get weather forecast 
+const getWeatherDetails = (cityName, lat, lon) => {
+    const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+    //get weather by 5 day forecast
+    fetch(WEATHER_API_URL).then(res => res.json()).then(data => {
+        const uniqueForecastDays = [];
+        const fiveDaysForecast = data.list.filter(forecast => {
+            const forecastDate = new Date(forecast.dt_txt).getDate();
+            if (!uniqueForecastDays.includes(forecastDate)) {
+                uniqueForecastDays.push(forecastDate);
+                return true;
+            }
+            return false;
+        });
+
+        //clears out input value        
+        cityInput.value = "";
+        currentWeatherDiv.innerHTML = "";
+        weatherCardsDiv.innerHTML = "";
+        //something has to be wrong with my api key because the forecast keeps logging as an empty array
+        //adding to DOM
+        fiveDaysForecast.forEach((weatherItem, index) => {
+            if (index === 0) {
+                currentWeatherDiv.insertAdjacentHTML("beforeend", createWeatherCard(cityName, weatherItem, index));
+            } else {
+                weatherCardsDiv.insertAdjacentHTML("beforeend", createWeatherCard(cityName, weatherItem, index));
+            }
+
+        });
+
+    }).catch(() => {
+        alert("Failed to fetch the forecast.");
+    });
+}
+
+const getCityCoordinates = () => {
+    const cityName = cityInput.value.trim();
+    if (!cityName) return;
+    const GEOCODING_API_URL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
+
+
+    fetch(GEOCODING_API_URL).then(res => res.json()).then(data => {
+        if (!data.length) return alert(`No coordinates found for ${cityName}`);
+        const { name, lat, lon } = data[0];
+        getWeatherDetails(name, lat, lon);
+    }).catch(() => {
+        alert("Failed to fetch the coordinates.");
+    })
+    // get city details and post them
+}
+
+searchButton.addEventListener("click", getCityCoordinates);
+
+//everything is working other than the actual weather populating.  issue has to be around the API 95% sure
